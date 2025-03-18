075158280d08538b25f482708bef1650b9cf795b
JoseAlberto23 <135029771+JoseAlberto23@users.noreply.github.com>
2025-03-17 21:23:37 -0300

    Add files via upload

---
+++ java05/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha o tipo de notificação:");
+        System.out.println("1: Email");
+        System.out.println("2: SMS");
+        System.out.println("3: Push Notification");
+
+        String choice = scanner.nextLine();1
+
+        System.out.println("Digite a mensagem a ser enviada:");
+        String message = scanner.nextLine();
+
+        Notification notification = NotificationFactory.createNotification(choice);
+        notification.send(message);
+
+        scanner.close();
+    }
+}
---
+++ java05/SMSNotification.java
@@ -0,0 +1,5 @@
+class SMSNotification implements Notification {
+    public void send(String message) {
+        System.out.println("Enviando SMS: " + message);
+    }
+}
